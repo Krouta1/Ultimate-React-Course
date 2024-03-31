1dd1187c3d85ccf19f7aa7ed3ab8860a6ecee9e2
Petr Kroutil <pkroutil1@seznam.cz>
2024-03-31 09:31:22 +0200

    part one

---
+++ 03-pizza-menu/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+  return (
+    <h1>Fast React Pizza Co.</h1>
+  )
+}
+
+export default Header
---
+++ 03-pizza-menu/src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import Pizza from './Pizza'
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+    </div>
+  )
+}
+
+export default Menu
