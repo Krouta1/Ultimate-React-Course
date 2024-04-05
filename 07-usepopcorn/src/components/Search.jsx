156ebbe9474567dd13b87c9c50216f31de8eed63
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-04-05 16:39:39 +0200

    09. usepopcorn

--- 07-usepopcorn/src/components/Search.jsx
+++ 07-usepopcorn/src/components/Search.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
-
-const Search = () => {
-  const [query, setQuery] = useState('');
-  return (
-    <input
-      className='search'
-      type='text'
-      placeholder='Search movies...'
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-    />
-  );
+const Search = ({ query, setQuery }) => {
+	return (
+		<input
+			className="search"
+			type="text"
+			placeholder="Search movies..."
+			value={query}
+			onChange={e => setQuery(e.target.value)}
+		/>
+	);
 };
 
 export default Search;
