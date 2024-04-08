a329e1afadc9a010373354237f3c72487d8f19ac
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-04-08 12:31:44 +0200

    07. UsePopcorn

--- 07-usepopcorn/src/components/WatchedMovie.jsx
+++ 07-usepopcorn/src/components/WatchedMovie.jsx
@@ -1,32 +1,32 @@
 /* eslint-disable react/prop-types */
 
 const WatchedMovie = ({ movie, onDeleteWatched }) => {
-  return (
-    <li key={movie.imdbID}>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>⭐️</span>
-          <span>{movie.imdbRating}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{movie.userRating}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{movie.runtime} min</span>
-        </p>
-        <button
-          className='btn-delete'
-          onClick={() => onDeleteWatched(movie.imdbID)}
-        >
-          ❌
-        </button>
-      </div>
-    </li>
-  );
+	return (
+		<li key={movie.imdbID}>
+			<img src={movie.Poster} alt={`${movie.Title} poster`} />
+			<h3>{movie.Title}</h3>
+			<div>
+				<p>
+					<span>⭐️</span>
+					<span>{movie.imdbRating}</span>
+				</p>
+				<p>
+					<span>🌟</span>
+					<span>{movie.userRating}</span>
+				</p>
+				<p>
+					<span>⏳</span>
+					<span>{movie.Runtime} min</span>
+				</p>
+				<button
+					className="btn-delete"
+					onClick={() => onDeleteWatched(movie.imdbID)}
+				>
+					❌
+				</button>
+			</div>
+		</li>
+	);
 };
 
 export default WatchedMovie;
