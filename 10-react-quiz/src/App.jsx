8fb149ef097cc743c90b2e0d4b5b6769994473db
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-04-08 17:07:38 +0200

    initial commit

---
+++ 10-react-quiz/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+import './index.css';
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
