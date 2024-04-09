e25386268b38d46ce0dfc983b48b15458f771f7c
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-04-09 19:53:32 +0200

    11. WorldWise part two

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -4,7 +4,7 @@ import Pricing from './pages/Pricing';
 import HomePage from './pages/HomePage';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
-import './index.css';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
         <Route path='/app' element={<AppLayout />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
