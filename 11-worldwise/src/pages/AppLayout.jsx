dc863c6718db6c845f1704ec0f92bd097a44cfe8
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-04-10 17:40:13 +0200

    Update AppLayout.jsx

--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -1,11 +1,14 @@
-import AppNav from '../components/AppNav';
+import Sidebar from '../components/Sidebar';
+import Map from '../components/Map';
+import styles from './AppLayout.module.css';
 
 const AppLayout = () => {
-  return (
-    <div>
-      <AppNav />
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<Sidebar />
+			<Map />
+		</div>
+	);
 };
 
 export default AppLayout;
