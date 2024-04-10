dce74ae3ed4f3ee2a996c1538b7048ae03f1325b
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-04-10 14:16:51 +0200

    Update App.jsx

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -5,20 +5,58 @@ import HomePage from './pages/HomePage';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
+import CityList from './components/CityList';
+import CountryList from './components/CountryList';
+import { useState, useEffect } from 'react';
+
+const BASE_URL = 'http://localhost:3001';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='product' element={<Product />} />
-        <Route path='pricing' element={<Pricing />} />
-        <Route path='login' element={<Login />} />
-        <Route path='/app' element={<AppLayout />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		async function fetchCities() {
+			try {
+				setIsLoading(true);
+				const res = await fetch(`${BASE_URL}/cities`);
+				const data = await res.json();
+				setCities(data);
+			} catch {
+				throw new Error('Data not found.');
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		fetchCities();
+	}, []);
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<HomePage />} />
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="login" element={<Login />} />
+				<Route path="/app" element={<AppLayout />}>
+					<Route
+						index
+						element={<CityList cities={cities} isLoading={isLoading} />}
+					/>
+					<Route
+						path="cities"
+						element={<CityList cities={cities} isLoading={isLoading} />}
+					/>
+					<Route
+						path="countries"
+						element={<CountryList cities={cities} isLoading={isLoading} />}
+					/>
+					<Route path="form" element={<p>Form</p>} />
+				</Route>
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
