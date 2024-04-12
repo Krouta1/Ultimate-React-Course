caf19203e0553ae7ed07100074f07f8f62de8d3a
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-04-12 16:00:09 +0200

    11. WorldWise auth part 1

--- 11-worldwise/src/components/User.jsx
+++ 11-worldwise/src/components/User.jsx
@@ -1,16 +1,15 @@
-import styles from "./User.module.css";
-
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+import { useFakeAuth } from '../contexts/FakeAuthContext';
+import styles from './User.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function User() {
-  const user = FAKE_USER;
+  const { user, logout } = useFakeAuth();
+  const navigate = useNavigate();
 
-  function handleClick() {}
+  function handleClick() {
+    logout();
+    navigate('/');
+  }
 
   return (
     <div className={styles.user}>
