060c32cf7502d41c65971aaa4e809502cdb040c8
Petr Kroutil <pkroutil1@seznam.cz>
2024-04-19 20:13:28 +0200

    REDUX

--- 14-redux-intro/src/store.js
+++ 14-redux-intro/src/store.js
@@ -1,12 +1,17 @@
-import { combineReducers, createStore } from 'redux';
-import accountReducer from './features/accounts/accountSlice';
-import customerReducer from './features/customers/customerSlice';
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 //CREATE STORE
 const rootReducer = combineReducers({
-	account: accountReducer,
-	customer: customerReducer,
+  account: accountReducer,
+  customer: customerReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 export default store;
 
 // const initialStateAccount = {
