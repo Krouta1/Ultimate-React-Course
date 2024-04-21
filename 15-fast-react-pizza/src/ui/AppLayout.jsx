618cbee8a5d68e3689e11773e8041fb1211b1f24
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-04-21 19:13:55 +0200

    app loading

--- 15-fast-react-pizza/src/ui/AppLayout.jsx
+++ 15-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,15 @@
 import Header from './Header';
+import Loader from './Loader';
 import CartOverview from '../features/cart/CartOverview';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 const AppLayout = () => {
+  const nav = useNavigation();
+  const isLoading = nav.state === 'loading';
+
   return (
-    <div>
+    <div className='layout'>
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
