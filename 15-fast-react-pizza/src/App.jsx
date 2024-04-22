2f8fdf7ff380b77d200811b6210f14db2e025112
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-04-22 07:53:46 +0200

    Update App.jsx

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -3,45 +3,48 @@ import Home from './ui/Home';
 import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, {
+	action as createOrderAction,
+} from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/menu',
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/order/new',
-        element: <CreateOrder />,
-      },
-      {
-        path: '/order/:orderId',
-        element: <Order />,
-        loader: orderLoader,
-        errorElement: <Error />,
-      },
-    ],
-  },
+	{
+		element: <AppLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: '/menu',
+				element: <Menu />,
+				loader: menuLoader,
+				errorElement: <Error />,
+			},
+			{
+				path: '/cart',
+				element: <Cart />,
+			},
+			{
+				path: '/order/new',
+				element: <CreateOrder />,
+				action: createOrderAction,
+			},
+			{
+				path: '/order/:orderId',
+				element: <Order />,
+				loader: orderLoader,
+				errorElement: <Error />,
+			},
+		],
+	},
 ]);
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
