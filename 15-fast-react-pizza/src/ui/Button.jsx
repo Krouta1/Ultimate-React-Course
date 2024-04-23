dde85a50a4422a16f801bc447b447893214a1943
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-04-23 17:38:43 +0200

    15. Menu styling

--- 15-fast-react-pizza/src/ui/Button.jsx
+++ 15-fast-react-pizza/src/ui/Button.jsx
@@ -2,21 +2,22 @@
 
 import { Link } from "react-router-dom";
 
-const Button = ({ children, disabled, to }) => {
+const Button = ({ children, disabled, to, type }) => {
+  const base =
+    "inline-block rounded-full bg-yellow-500 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-200 hover:bg-yellow-400 focus:bg-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50";
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
   if (to)
     return (
-      <Link
-        to={to}
-        className=" inline-block rounded-full bg-yellow-500 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-200 hover:bg-yellow-400 focus:bg-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:px-6 sm:py-4"
-      >
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button
-      disabled={disabled}
-      className=" inline-block rounded-full bg-yellow-500 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-200 hover:bg-yellow-400 focus:bg-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:px-6 sm:py-4"
-    >
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
