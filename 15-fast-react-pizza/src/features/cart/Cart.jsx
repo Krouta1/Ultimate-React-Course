6bcfd3e683735c8ca68ec8d7950fcaee59d1c96a
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-04-24 07:22:28 +0200

    Update Cart.jsx

--- 15-fast-react-pizza/src/features/cart/Cart.jsx
+++ 15-fast-react-pizza/src/features/cart/Cart.jsx
@@ -4,34 +4,40 @@ import { useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
 import CartItem from "./CartItem";
+import EmptyCart from "./EmptyCart"
+import {getCart,clearCart} from "./cartSlice"
+import { useDispatch } from "react-redux";
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+// const fakeCart = [
+//   {
+//     pizzaId: 12,
+//     name: "Mediterranean",
+//     quantity: 2,
+//     unitPrice: 16,
+//     totalPrice: 32,
+//   },
+//   {
+//     pizzaId: 6,
+//     name: "Vegetale",
+//     quantity: 1,
+//     unitPrice: 13,
+//     totalPrice: 13,
+//   },
+//   {
+//     pizzaId: 11,
+//     name: "Spinach and Mushroom",
+//     quantity: 1,
+//     unitPrice: 15,
+//     totalPrice: 15,
+//   },
+// ];
 
 function Cart() {
-  const cart = fakeCart;
+  const dispatch = useDispatch()
+  const cart = useSelector(getCart)
   const username = useSelector((state) => state.user.username);
+
+  if(!cart.length) return <EmptyCart/>
 
   return (
     <div className="px-4 py-3">
@@ -40,15 +46,15 @@ function Cart() {
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
       <ul className=" mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <CartItem key={item.key} item={item} />
+          <CartItem key={item.pizzaId} item={item} />
         ))}
       </ul>
 
       <div className=" mt-6 space-x-2">
-        <Button to="/order/new" type={"primary"}>
+        <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={()=>dispatch(clearCart())}>Clear cart</Button>
       </div>
     </div>
   );
