289072c65dd529ce65725ab875b474ec89128205
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-04-27 10:37:26 +0200

    16. global styles

--- 16-wild-oasis/src/App.jsx
+++ 16-wild-oasis/src/App.jsx
@@ -1,33 +1,10 @@
-import styled from 'styled-components';
-
-const H1 = styled.h1`
-  font-size: 2rem;
-  font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1rem;
-  padding: 0.5rem 1rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 5px;
-  background-color: purple;
-  color: white;
-`;
-const StyledApp = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #f0f0f0;
-`;
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <StyledApp>
-      <H1>Hello</H1>
-      <Button>Click Me</Button>
-    </StyledApp>
+    <>
+      <GlobalStyles />
+    </>
   );
 }
 
