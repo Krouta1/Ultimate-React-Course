049b3031648cb69ad7505591d9ca3a694c640ef6
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-04-30 11:31:04 +0200

    16. image upload

--- 16-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 16-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -30,7 +30,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
   function onError(errors) {
     // console.error(errors);
--- 16-wild-oasis/src/services/apiCabins.js
+++ 16-wild-oasis/src/services/apiCabins.js
@@ -12,13 +12,33 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${
+    import.meta.env.VITE_SUPABASE_BUCKET_URL
+  }/storage/v1/object/cabin-images/${imageName}`;
+  //1. Create a new cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }]);
 
   if (error) {
     console.log("Could not create cabin", error.message);
     throw new Error(error.message);
   }
 
+  //2. upload the image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. delete cabin if image upload fails
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    throw new Error(storageError.message);
+  }
   return data;
 }
 
