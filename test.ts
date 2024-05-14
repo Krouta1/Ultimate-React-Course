29e33ad667a8d082705b42f9f7c4231a5d20f508
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-05-14 20:00:12 +0200

    next work commit

--- test.ts
+++ test.ts
@@ -1,40 +1,26 @@
-// přípraví defaultní položky menu a rozdělí je na dvě části, aby se mohly vkládat další položky mezi ně
-function pripravDefaultPolozkyMenu(defaultPolozky: string[], vlozitPolozkyZaPrvek: number) {
-    const prvniCastMenu = defaultPolozky.slice(0, vlozitPolozkyZaPrvek);
-    const druhaCastMenu = defaultPolozky.slice(vlozitPolozkyZaPrvek);
-    return { prvniCastMenu, druhaCastMenu };
-}
+// Vytáhnu z řádku "Kod", Slovnik.Loc(823805), "string", 1 a vytvořím pole ["Kod", "Slovnik.Loc(823805)", "string", 1]
+const lines = [
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+  'new AttributeInfo("Kod", Slovnik.Loc(823805), "string", 1), // Kód',
+];
 
-// přípraví proměnné pro položky menu
-function pripravPromennePolozkyMenu(zobrazit: boolean, nadrizenyUzelProMenu:string, polozkyMenu: string[]) {
-    return { zobrazit,nadrizenyUzelProMenu,polozkyMenu };
-}
+const numArrays = lines.map((line) => {
+  const firstParen = line.indexOf("(");
+  const lastParen = line.lastIndexOf(")");
+  const extracted = line.substring(firstParen + 1, lastParen);
 
-// vrátí položky menu podle podmíněných položek, tato metoda nepočítá s tím, že by menu mohlo být více jak 2 úrovné
-function vratPolozkyMenu(defaultniPolozkyMenu:string[], prvniCastMenu:string[], druhaCastMenu:string[], podminenePolozkyMenu:any[]) {
-    const nalezenePolozkyMenu = podminenePolozkyMenu.filter(i => i.zobrazit === true);
-     
-    if (nalezenePolozkyMenu.length === 0) {
-        return [...prvniCastMenu,...druhaCastMenu]
-    } else if (nalezenePolozkyMenu.length === 1) {
-        return [...prvniCastMenu,...nalezenePolozkyMenu[0].polozkyMenu,...druhaCastMenu]
-    } else {
-        const polozky = nalezenePolozkyMenu.flatMap(item => [
-            item.nadrizenyUzelProMenu,
-            ...item.polozkyMenu.map(polozka=> polozka + "_změněná")
-        ])
-        return [...prvniCastMenu,...polozky,...druhaCastMenu]
-    }
-}
-
-
-let defaultniPolozkyMenu = ["a", "b", "c", "d", "e", "f"];
-const {prvniCastMenu, druhaCastMenu } = pripravDefaultPolozkyMenu(defaultniPolozkyMenu, 4);
-
-const byty = pripravPromennePolozkyMenu(false, "BYTY", ["1", "2", "3", "4", "5"]);
-const nebyty = pripravPromennePolozkyMenu(false, "NEBYTY", ["6", "7", "8", "9", "10"]);
-const komerce = pripravPromennePolozkyMenu(true, "KOMERCE", ["11", "12", "13", "14", "15"]);
-const rn = pripravPromennePolozkyMenu(false, "RN", ["16", "17", "18", "19", "20"]);
-const podminenePolozkyMenu = [byty,nebyty,komerce,rn];
-const vysledek = vratPolozkyMenu(defaultniPolozkyMenu, prvniCastMenu, druhaCastMenu, podminenePolozkyMenu);
- 
+  return extracted.split(",").map((item) => {
+    item = item.trim();
+    const locMatch = item.match(/Slovnik\.Loc\((\d+)\)/);
+    return locMatch ? locMatch[1] : item;
+  });
+});
